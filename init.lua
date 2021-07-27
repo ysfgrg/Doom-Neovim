57f81f229885050710833a8fb9498088da09fe98
ysfgrg <85966348+ysfgrg@users.noreply.github.com>
2021-07-27 23:10:41 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- load all plugins
+require "pluginList"
+
+local g = vim.g
+
+g.mapleader = " "
+g.auto_save = false
+
+require "statusline"
+require "top-bufferline"
+require "telescope-conf"
+require "dashboard-conf"
+require "general"
+require('onedark').setup()
+require("which-key").setup {}
+
+vim.g.onedark_style = 'darker'
+vim.cmd("source ~/.config/nvim/general.vim")
+vim.cmd("source ~/.config/nvim/mappings.vim")
